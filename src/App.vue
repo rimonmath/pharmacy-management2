22ea5a76a34d69e46717bcb99ad95c5de55bf1b1
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2024-02-29 10:41:40 +0600

    Part1-3

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const playList = 'Vue JS composition API';
+
+/*=======================*/
+const completedVideos = ref(3);
+function completeVideo() {
+  completedVideos.value++;
+}
+/*=======================*/
+
+/*=======================*/
+const firstName = ref('');
+const lastName = ref('');
+
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`;
+});
+
+function getCertificate() {
+  alert(
+    `Welcome ${fullName.value}.
+        You have completed total ${completedVideos.value} videos
+        of vue js 3 composition API course.`
+  );
+}
+
+onMounted(() => {
+  firstName.value = 'Guest';
+});
+/*=======================*/
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ playList }}</h1>
+    <p>
+      Completed Videos <strong>{{ completedVideos }}</strong>
+      <button class="ml-4" @click="completeVideo">+</button>
+    </p>
+
+    <hr />
+
+    <input type="text" v-model="firstName" placeholder="First Name" />
+    <input
+      class="ml-4"
+      type="text"
+      v-model="lastName"
+      placeholder="Last Name"
+    />
+
+    <br />
+    <br />
+    <button @click="getCertificate">Get certificate</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 555px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 200px;
+}
+
+.ml-4 {
+  margin-left: 11px;
+}
+</style>
