03d8ce80427aaa3ac42e67da4bc00794b4bc133e
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2024-02-29 13:01:23 +0600

    part5 done

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,7 @@
-<script setup>
-import { ref, computed, onMounted } from 'vue';
-import TheButton from '@/components/TheButton.vue';
-
-const playList = 'Vue JS composition API';
-
-/*=======================*/
-const completedVideos = ref(3);
-function completeVideo() {
-  completedVideos.value++;
-}
-/*=======================*/
-
-/*=======================*/
-const firstName = ref('');
-const lastName = ref('');
-
-const fullName = computed(() => {
-  return `${firstName.value} ${lastName.value}`;
-});
-
-function getCertificate() {
-  alert(
-    `Welcome ${fullName.value}.
-        You have completed total ${completedVideos.value} videos
-        of vue js 3 composition API course.`
-  );
-}
-
-onMounted(() => {
-  firstName.value = 'Guest';
-});
-/*=======================*/
-</script>
+<script setup></script>
 
 <template>
-  <div class="container">
-    <h1>{{ playList }}</h1>
-    <p>
-      Completed Videos <strong>{{ completedVideos }}</strong>
-      <button class="ml-4" @click="completeVideo">+</button>
-    </p>
-
-    <hr class="my-4" />
-
-    <input type="text" v-model="firstName" placeholder="First Name" />
-    <input
-      class="ml-4"
-      type="text"
-      v-model="lastName"
-      placeholder="Last Name"
-    />
-
-    <br />
-    <br />
-    <TheButton block @click="getCertificate">Get certificate</TheButton>
-  </div>
+  <router-view></router-view>
 </template>
 
-<style scoped>
-.container {
-  width: 555px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 200px;
-}
-
-.ml-4 {
-  margin-left: 11px;
-}
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/Login.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: LoginView
     },
     {
       path: '/about',
@@ -18,6 +19,6 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     }
   ]
-})
+});
 
-export default router
+export default router;
