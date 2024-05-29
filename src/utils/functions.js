ae610147a86f13eb366a28ee4383b2c2d389dd6f
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2024-05-29 08:06:08 +0600

    funxtions formatted

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,27 +1,27 @@
-import { eventBus } from "./eventBus";
+import { eventBus } from './eventBus';
 
 export const showErrorMessage = (error) => {
-  let errorMessage = error || "Something went wrong!";
+  let errorMessage = error || 'Something went wrong!';
 
   if (error.response) {
     errorMessage = error.response.data.message;
   }
 
-  eventBus.emit("toast", {
-    type: "Error",
+  eventBus.emit('toast', {
+    type: 'Error',
     message: errorMessage
   });
 };
 
 export const showSuccessMessage = (payload) => {
-  let successMessage = payload || "Success";
+  let successMessage = payload || 'Success';
 
   if (payload.data) {
     successMessage = payload.data.message;
   }
 
-  eventBus.emit("toast", {
-    type: "Success",
+  eventBus.emit('toast', {
+    type: 'Success',
     message: successMessage
   });
 };
